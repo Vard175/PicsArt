c2b113feb913bc95a817135982df1d81b5314fb2
Vard175 <galustyanvarduhi@gmail.com>
2021-03-27 17:20:27 +0400

    add

---
+++ Homework1/src/PrimitiveTypes.java
@@ -0,0 +1,24 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        byte b=4;
+        short s =32767;
+        int i = - 2147483648;
+        long l=48454632513231L;
+        float f=485656.8985f;
+        double d=4584555555555.484848488;
+        boolean t=true;
+        char c1='L';
+        char c2= '\u00a3';
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(t);
+        System.out.println(c1);
+        System.out.println(c2);
+    }
+}
